699d5f089a062de3cccafd6b6cac6938f4efd918
homile <ssanho2@naver.com>
2022-11-28 21:05:09 +0900

    feat:portfolio 페이지 중첩 경로 설정

---
+++ nextjs-course/pages/portfolio/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// 포트폴리오 페이지 생성(중첩 경로)
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>PortfolioPage</h1>
+    </div>
+  );
+};
+
+export default PortfolioPage;
