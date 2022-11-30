45753febb5e7cc88df2398a0af3df339d901527e
homile <ssanho2@naver.com>
2022-11-30 16:02:40 +0900

    feat:동적 라우팅 생성

---
+++ nextjs-course/pages/portfolio/[projectid].js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// 대괄호를 사용하여 동적 라우팅을 지정한다.
+// /portfolio/one, /portfolio/hello를 작성해도 해당 컴포넌트 페이지로 이동한다.
+// /portfolio/list를 작성하면 list 페이지로 이동한다.
+const PortfolioProjectPage = () => {
+  return (
+    <div>
+      <h1>PortfolioProjectPage</h1>
+    </div>
+  );
+};
+
+export default PortfolioProjectPage;
