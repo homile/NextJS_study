85c7d899a9700b649a9e43e1cc900eb3e753180c
homile <ssanho2@naver.com>
2022-12-01 16:02:57 +0900

    feat:동적 경로이동 설정

--- nextjs-course/pages/clients/index.js
+++ nextjs-course/pages/clients/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 // 중첩 동적 라우트 경로: /clients
 const ClientPage = () => {
+  // 동적 경로이동 활용 클라이언트에 대한 정보를 나열할때 (마이페이지?)
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "menu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
