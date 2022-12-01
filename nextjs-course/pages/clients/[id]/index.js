34295b26e7087f900500f5d1d32f8b9397d2076a
homile <ssanho2@naver.com>
2022-12-01 19:08:22 +0900

    feat:버튼으로 페이지를 이동하는 방법 및 이유

--- nextjs-course/pages/clients/[id]/index.js
+++ nextjs-course/pages/clients/[id]/index.js
@@ -7,9 +7,16 @@ const ClientProjectsPage = () => {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    // load data...
+    // form을 작성하고 제출 후 페이지를 이동할 때 사용한다.
+    router.push("/clients/max/project");
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Laod Project A</button>
     </div>
   );
 };
