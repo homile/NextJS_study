03d17615e0e0294ca2da7c35198f7194539a8c13
homile <ssanho2@naver.com>
2022-12-03 15:10:05 +0900

    style:event 카드 아이콘 추가

--- nextjs-course-section4/components/events/EventItem.js
+++ nextjs-course-section4/components/events/EventItem.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import classes from "./EventItem.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -27,14 +30,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon></AddressIcon>
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
