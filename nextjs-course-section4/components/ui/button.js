1d1d0627ee674d734154b8f3c0f387aa253eba92
homile <ssanho2@naver.com>
2022-12-04 21:14:06 +0900

    feat: 페이지 이동 기능과 form 제출 기능 2개로 분할

--- nextjs-course-section4/components/ui/button.js
+++ nextjs-course-section4/components/ui/button.js
@@ -3,11 +3,21 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 const Button = (props) => {
+  // props중 link가 있을 경우 화면 이동 기능
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        {/* Link 태그안에 a 태그를 추가하는 이유를 알아보시요. */}
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
+  // porps에 link가 없을 경우 form 전송 기능
   return (
-    <Link href={props.link}>
-      {/* Link 태그안에 a 태그를 추가하는 이유를 알아보시요. */}
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 };
 
