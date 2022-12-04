8754875a84f37dc4b1321853c94d2d466ac05ab6
homile <ssanho2@naver.com>
2022-12-04 21:49:58 +0900

    feat:결과 제목 컴포넌트 추가

--- nextjs-course-section4/pages/events/[...slug].js
+++ nextjs-course-section4/pages/events/[...slug].js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
 
 const FilteredEventsPage = () => {
   const router = useRouter();
@@ -39,7 +40,7 @@ const FilteredEventsPage = () => {
 
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 };
