eeb8cb57d17424ed0bd352e4b5703bda4aca6122
homile <ssanho2@naver.com>
2022-12-04 21:34:48 +0900

    feat:연/월 필터 기능 추가

--- nextjs-course-section4/pages/events/index.js
+++ nextjs-course-section4/pages/events/index.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEvnetsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    // slug 방식으로 중첩 동적 경로 설정
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
