1a7907b24b977daee675cf217b39079fe521a410
homile <ssanho2@naver.com>
2022-12-04 20:37:25 +0900

    feat:nav바 전체 레이아웃에 랜더링 적용

--- nextjs-course-section4/pages/_app.js
+++ nextjs-course-section4/pages/_app.js
@@ -1,9 +1,16 @@
+import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 // css 파일을 추가할 수 있다.
 // next에는 css 모듈이 내장되어 있다.
+
+// 루트 컴포넌트 파일로 여러 페이지 컴포넌트가 렌더링 되는 곳이다.
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
