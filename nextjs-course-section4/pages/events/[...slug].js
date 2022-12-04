71f0389e0c6418237921a3596e5ae9f6036ee50b
homile <ssanho2@naver.com>
2022-12-04 21:47:34 +0900

    faet:중첩 동적 경로 필터에 따른 랜더링 화면 구현

--- nextjs-course-section4/pages/events/[...slug].js
+++ nextjs-course-section4/pages/events/[...slug].js
@@ -1,6 +1,42 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
 
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  // data가 없을 경우
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  // url에 있는 query가 연/월이 아닐 경우
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please abjiust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  // 검색 결과가 없을 경우
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
