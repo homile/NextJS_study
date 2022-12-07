2e90f8708d688d613a80686a5c261c684b457bbe
homile <ssanho2@naver.com>
2022-12-07 16:45:36 +0900

    fix:getStaticProps 하드코딩 -> 파일 데이터 받아오기

--- section5/pages/index.js
+++ section5/pages/index.js
@@ -1,3 +1,8 @@
+// 파일 시스템모듈
+// 브라우저 측 js가 파일 시스템에 접근할 수 없기 때문에 사용한다.
+import fs from "fs";
+import path from "path";
+
 const HomePage = (props) => {
   const { products } = props;
 
@@ -14,9 +19,13 @@ const HomePage = (props) => {
 // 일반적인 React에서는 useEffect를 사용했음.
 // CSR 방식이 아닌 SSR 방식이기 때문에 사용자가 볼 수 없는 크리덴셜(credential)을 사용할 수 있다.
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
   };
 }
