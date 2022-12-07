e004c0fe6ed0724ef68117e7c98251a84aea51b6
homile <ssanho2@naver.com>
2022-12-07 16:30:23 +0900

    feat:getStaticProps 하드코딩 구현

--- section5/pages/index.js
+++ section5/pages/index.js
@@ -1,11 +1,24 @@
 const HomePage = (props) => {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 };
+
+// 데이터를 사전 패칭하는 함수
+// 일반적인 React에서는 useEffect를 사용했음.
+// CSR 방식이 아닌 SSR 방식이기 때문에 사용자가 볼 수 없는 크리덴셜(credential)을 사용할 수 있다.
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
+}
 
 export default HomePage;
